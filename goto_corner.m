3821cbda2f01fdb49ba3ef9a8fce21013313022a
skhokhlov <worldfly@yandex.ru>
2014-10-22 14:48:36 +0400

    Оптимизация

--- goto_corner.m
+++ goto_corner.m
@@ -1,10 +1,6 @@
 function goto_corner(r, side1, side2)
 %Дано: Поле с роботом
 %Результат: Робот в юго-западном угле
-    if side1 == 0 || side2 == 0
-        side1 = 's';
-        side2 = 'w';
-    end
     while r.is_bord(side1) == 0 || r.is_bord(side2) == 0
         if r.is_bord(side1) == 0
             r.step(side1)
